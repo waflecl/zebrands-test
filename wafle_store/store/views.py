449f3caccda3092770f1dc1277826bd836a4e2da
Eric Zepeda <eric.zepeda@tcglatam.com>
2021-04-17 19:59:22 -0400

    [ADD] [WORKING] Templates

--- wafle_store/store/views.py
+++ wafle_store/store/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from store.models import Category
 
-# Create your views here.
+
+def commonContext( dictParams=None):
+  categories = Category.objects.all()
+  context = {
+    'categories':categories
+  }
+
+  if dictParams != None :
+    context.update(dictParams)
+  return context
+
 def index(request):
-  return render(request, 'index.html', {})
+  context = commonContext()
+  return render(request, 'index.html', context)
+
