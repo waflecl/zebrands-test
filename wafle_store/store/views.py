3701e8e706026f78b4f748959de415b256ce79aa
Eric Zepeda <eric.zepeda@tcglatam.com>
2021-04-17 20:48:00 -0400

    [WORKING] On templates

--- wafle_store/store/views.py
+++ wafle_store/store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from store.models import Category
+from store.models import Category, Product
 
 
 def commonContext( dictParams=None):
@@ -9,10 +9,18 @@ def commonContext( dictParams=None):
   }
 
   if dictParams != None :
-    context.update(dictParams)
+    if dictParams[0] :
+      context.update(
+        {dictParams[0]._meta.model.__name__:dictParams}
+      )
   return context
 
 def index(request):
-  context = commonContext()
+  products = Product.objects.all()
+  context = None
+  if products : 
+    context = commonContext(products)
+  else:
+    context = commonContext()
   return render(request, 'index.html', context)
 
