574fe18c6c0e248f6bf06ec751bacda68f623d95
Eric Zepeda <eric.zepeda@tcglatam.com>
2021-04-17 20:22:24 -0400

    [FIX]Renaming all project

--- wafle_store/store_system/urls.py
+++ wafle_store/store_system/urls.py
@@ -1,4 +1,4 @@
-"""wafle_store URL Configuration
+"""store_system URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
