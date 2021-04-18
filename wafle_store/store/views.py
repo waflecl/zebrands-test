7387000477e9914649864d31aa0fbacbf7c34708
Eric Zepeda <eric.zepeda@tcglatam.com>
2021-04-18 00:14:11 -0400

    [WORKING] Templates

--- wafle_store/store/views.py
+++ wafle_store/store/views.py
@@ -2,40 +2,35 @@ from django.http import Http404
 from django.shortcuts import render
 from store.models import Category, Product
 
+def commonContext( dict_params=None):
 
-def commonContext( dictParams=None):
+  context = {}
   categories = Category.objects.all()
-  context = {
-    'categories':categories
-  }
-
-  if dictParams != None :
-    if dictParams[0] :
-      context.update(
-        {dictParams[0]._meta.model.__name__:dictParams}
-      )
+  context.update({'categories' : categories})
+  if dict_params != None :
+      context.update(dict_params)
   return context
 
 def index(request):
   products = Product.objects.all()
-  context = None
-  if products != None: 
-    context = commonContext(products)
-  else:
-    context = commonContext()
-  return render(request, 'index.html', context)
+  dictionary = {'products':products}
+  return render(request, 'index.html', dictionary)
 
 def product(request, slug):
   try:
-    product = Product.objects.filter(slug=slug)
+    product = Product.objects.get(slug=slug)
+    dictionary = {'product':product}
   except Product.DoesNotExist:
     raise Http404("Product doesn't exist")
-  return render(request, 'product.html', commonContext(product))
+  return render(request, 'product.html', commonContext(dictionary))
 
 def category(request, slug):
   try:
-    category = Category.objects.filter(slug=slug)
+    category = Category.objects.get(slug=slug)
+    products = Product.objects.filter(categories=category)
+    dictionary = {'category':category, 'products':products}
   except Category.DoesNotExist:
     raise Http404("Category doesn't exist")
-  return render(request, 'category.html', commonContext(category))
+  
+  return render(request, 'category.html', commonContext(dictionary))
 
