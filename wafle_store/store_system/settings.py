faf5490e6f6ba6e5ca7261e95676917f84cf1219
Eric Zepeda <eric.zepeda@tcglatam.com>
2021-04-18 00:51:30 -0400

    [WORKING] Fronend view store

--- wafle_store/store_system/settings.py
+++ wafle_store/store_system/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store',
-    'debug_toolbar'
+    'store'
 ]
 
 MIDDLEWARE = [
@@ -49,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'store_system.urls'
